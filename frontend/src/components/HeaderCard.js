212fe2e916ffd5569f68706592ab39b92edf5669
Ronny <gsingh16742@gmail.com>
2022-10-15 14:49:03 +0530

    reslved image error

--- frontend/src/components/HeaderCard.js
+++ frontend/src/components/HeaderCard.js
@@ -10,19 +10,19 @@ const cardStyle={
 
 const HeaderCard = () => {
   return (
-    <div>
-      <div className="row m-0" style={{height:"100vh",width:"100vw",background:`#ffffff url(${image}) no-repeat`,backgroundSize:"cover"}}>
-  <div className="col-sm-6  ms-5 " >
+    <>
+      <div className="row m-0" style={{background:`#ffffff url(${image}) no-repeat`,backgroundSize:"cover"}}>
+  <div className="col-sm-6 ms-5" >
     <div className="card border-0" style={cardStyle}>
       <div className="card-body">
         <h1 className="card-title ">Wellness Builds Upon the Medical</h1>
         <p className="card-text fs-5 text-lg-start">Our spcialist always aspire to bridge the gap between your benefits plan and the health needs of your family.</p>
-        <a href="#" className="btn btn-primary">Learn More</a>
+        <a href="/" className="btn btn-primary">Learn More</a>
       </div>
     </div>
   </div>
 </div>
-    </div>
+    </>
   )
 }
 
