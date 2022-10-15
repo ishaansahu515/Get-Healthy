65671ecaefdee00241e279fcec772bb95bbbcd3e
Ronny <gsingh16742@gmail.com>
2022-10-15 23:36:00 +0530

    added predictions component

--- frontend/src/pages/ServicePage.js
+++ frontend/src/pages/ServicePage.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Prediction from '../components/feature/Prediction'
 import Navbar from '../components/Navbar'
 
 const ServicePage = () => {
   return (
     <>
       <Navbar/>
+      <Prediction />
     </>
   )
 }
