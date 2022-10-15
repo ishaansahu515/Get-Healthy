305de0f973948ce32db26976e045f8d5b07b2850
ishan51551 <ishaansahu515@gmail.com>
2022-10-15 11:18:56 +0530

    added home page components

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
   <div className="container-fluid">
     <a className="navbar-brand" href="/">Get-Healthy</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import HeaderCard from '../components/HeaderCard'
 import Navbar from '../components/Navbar'
 
 const HomePage = () => {
   return (
-    <><Navbar/></>
+    <>
+    <Navbar/>
+    <HeaderCard/>
+    </>
   )
 }
 
