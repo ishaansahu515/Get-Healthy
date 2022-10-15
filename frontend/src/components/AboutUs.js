e10b3d869e17941d745826d79a3ddee07febd5b8
ishan51551 <ishaansahu515@gmail.com>
2022-10-15 18:31:42 +0530

    aboutus

--- frontend/src/components/AboutUs.js
+++ frontend/src/components/AboutUs.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 function AboutUs () {
   return (
+    <div className=''>
     <p class="text-center m-5">A healthy lifestyle keeps you fit, energetic and at reduced risk for disease. According to WHO, Healthy living is a way of living that helps you enjoy more aspects of your life. It is a way of living that lowers the risk of being seriously ill or dying early. Health is not just about avoiding a disease or illness.</p>
-  
+  </div> 
   )
 }
 
