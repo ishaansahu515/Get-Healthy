7aa5c4dba30e26495d252598b01bcc5f1fcd66d4
ishan51551 <ishaansahu515@gmail.com>
2022-10-15 14:16:55 +0530

    staging changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import HomePage from "./pages/HomePage";
+import { BrowserRouter as Router, Routes,Route} from "react-router-dom";
+import ServicePage from "./pages/ServicePage";
+import About from "./pages/About";
 
 function App() {
   return (
-    <div className="App">
-      <HomePage/>
-      
+    <div className="App" >
+      <Router>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/services" element={<ServicePage/>} />
+        <Route path="/About" element={<About/>} />
+       </Routes>
+      </Router>
     </div>
   );
 }
